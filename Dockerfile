730684020c4e6112e34f27472929339914f46b7f
nikita <nikgrygoryev@gmail.com>
2023-05-17 12:13:45 +0200

    generation and prepare evaluation

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,21 @@
 FROM nvidia/cuda:12.0.0-runtime-ubuntu22.04
 FROM python:3.10
-RUN pip install --upgrade pip --trusted-host pypi.org --trusted-host files.pythonhosted.org
-COPY requirements.txt .
-RUN pip install -r requirements.txt
 
-COPY data/en_es_dataset.json data/en_es_dataset.json
-COPY train_config.json .
-COPY finetune.py .
-COPY pyproject.toml .
+ARG MODEL
+WORKDIR /home/bloom_lora_finetune
+
+RUN pip install --upgrade pip --trusted-host pypi.org --trusted-host files.pythonhosted.org
+COPY requirements.txt /home/bloom_lora_finetune
+RUN pip install -r /home/bloom_lora_finetune/requirements.txt
+RUN curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+RUN apt-get install git-lfs
+
+
+RUN git clone https://huggingface.co/bigscience/$MODEL
+
+COPY data/train_tilde.json /home/bloom_lora_finetune/data/train_tilde.json
+COPY train_config.json ./home/bloom_lora_finetune
+COPY finetune.py ./home/bloom_lora_finetune
 
 
 
--- generate_bloom.py
+++ generate_bloom.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 
+import pandas as pd
 import torch
 from peft import PeftModel
 import transformers
@@ -22,10 +23,18 @@ def get_config(file: str = "generate_config.json"):
         return json.load(f)
 
 
+def get_dataset(file):
+    with open(file, "r", encoding="utf8") as f:
+        return json.load(f)
+
+
 def generate():
+
     tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom")
     data = get_config()
+
     model_path = data["model_path"]
+
     lora_weights = data["lora_weights"]
     load_in_8bit = data["load_in_8bit"]
     temperature = data["temperature"]
@@ -33,8 +42,8 @@ def generate():
     top_k = (data["top_k"],)
     num_beams = (data["num_beams"],)
     max_new_tokens = (data["max_new_tokens"],)
-    input = data["input"]
-    device = "cpu"
+    test_path = data["test_path"]
+    device = data["device"]
     if torch.cuda.is_available():
         device = "cuda"
 
@@ -43,6 +52,9 @@ def generate():
             device = "mps"
     except:
         pass
+
+    # Load test dataset
+    test_data = get_dataset(test_path)
 
     if device == "cuda":
         model = BloomForCausalLM.from_pretrained(
@@ -76,51 +88,69 @@ def generate():
             device_map={"": device},
         )
 
-    def generate_prompt(instruction, input=None):
+    # ToDo: create a list of prompts or ask user via API
+    def generate_prompt(instruction, input_=None):
         if input:
-            return f"""### Instruction: {instruction}\n\n### Input: {input}\n\n### Response: """
+            return f"""### Instruction: {instruction}\n\n### Input: {input_}\n\n### Response: """
         else:
             return f"""### Instruction: {instruction}\n\n### Response:"""
 
     model.eval()
     if torch.__version__ >= "2":
         model = torch.compile(model)
-
-    prompt = generate_prompt(instruction, input)
-    inputs = tokenizer(prompt, return_tensors="pt")
-    input_ids = inputs["input_ids"].to(device)
-    generation_config = GenerationConfig(
-        temperature=temperature,
-        top_p=top_p,
-        top_k=top_k,
-        num_beams=num_beams,
-    )
-    with torch.no_grad():
-        generation_output = model.generate(
-            input_ids=input_ids,
-            generation_config=generation_config,
-            return_dict_in_generate=True,
-            output_scores=True,
-            max_new_tokens=max_new_tokens,
+    output = []
+    for data in test_data:
+        instruction = data["instruction"]
+        input_ = data["input"]
+        prompt = generate_prompt(instruction, input_=input_)
+        inputs = tokenizer(prompt, return_tensors="pt")
+        input_ids = inputs["input_ids"].to(device)
+        generation_config = GenerationConfig(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            num_beams=num_beams,
         )
-    s = generation_output.sequences[0]
-    output = tokenizer.decode(s)
-    return output.split("### Response:")[1].strip()
+        with torch.no_grad():
+            generation_output = model.generate(
+                input_ids=input_ids,
+                generation_config=generation_config,
+                return_dict_in_generate=True,
+                output_scores=True,
+                max_new_tokens=max_new_tokens,
+            )
+
+        s = generation_output.sequences[0]
+        gen_output = tokenizer.decode(s).split("### Response:")[1].strip()
+        output.append(
+            {
+                "instruction": instruction,
+                "input": input_,
+                "expected_output": data["output"],
+                "generated_output": gen_output
+            }
+        )
+
+    return output
 
 
 if __name__ == "__main__":
     # testing code for readme
-    for instruction in [
-        "Tell me about alpacas.",
-        "Tell me about the president of Mexico in 2019.",
-        "Tell me about the king of France in 2019.",
-        "List all Canadian provinces in alphabetical order.",
-        "Write a Python program that prints the first 10 Fibonacci numbers.",
-        "Write a program that prints the numbers from 1 to 100. But for multiples of three print 'Fizz' instead of the number and for the multiples of five print 'Buzz'. For numbers which are multiples of both three and five print 'FizzBuzz'.",
-        "Tell me five words that rhyme with 'shock'.",
-        "Translate the sentence 'I have no mouth but I must scream' into Spanish.",
-        "Count up from 1 to 500.",
-    ]:
-        print("Instruction:", instruction)
-        print("Response:", generate())
-        print()
+    generate()
+    #
+    #
+    #
+    # for instruction in [
+    #     "Tell me about alpacas.",
+    #     "Tell me about the president of Mexico in 2019.",
+    #     "Tell me about the king of France in 2019.",
+    #     "List all Canadian provinces in alphabetical order.",
+    #     "Write a Python program that prints the first 10 Fibonacci numbers.",
+    #     "Write a program that prints the numbers from 1 to 100. But for multiples of three print 'Fizz' instead of the number and for the multiples of five print 'Buzz'. For numbers which are multiples of both three and five print 'FizzBuzz'.",
+    #     "Tell me five words that rhyme with 'shock'.",
+    #     "Translate the sentence 'I have no mouth but I must scream' into Spanish.",
+    #     "Count up from 1 to 500.",
+    # ]:
+    #     print("Instruction:", instruction)
+    #     print("Response:", generate())
+    #     print()
